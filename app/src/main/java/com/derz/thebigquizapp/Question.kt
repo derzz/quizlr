59bdd55e90643fa611495a0f382a2f593df42ffc
Daniel Zhang <danielrzhang@gmail.com>
2023-06-08 10:12:33 -0400

    Created new Question class.

---
+++ app/src/main/java/com/derz/thebigquizapp/Question.kt
@@ -0,0 +1,31 @@
+package com.derz.thebigquizapp
+
+class Question(private val question: String, private var answers: MutableList<String>,
+               private val correctAnswer: String, private var isAnswered: Boolean) {
+
+    // Randomize answers
+    fun randomizeAnswers() {
+        this.answers.shuffle()
+    }
+
+    // Getter and setter functions
+    fun getQuestion(): String {
+        return this.question
+    }
+
+    fun getAnswers(): MutableList<String> {
+        return this.answers
+    }
+
+    fun getCorrectAnswer(): String {
+        return this.correctAnswer
+    }
+
+    fun getIsAnswered(): Boolean {
+        return this.isAnswered
+    }
+
+    fun setIsAnswered(isAnswered: Boolean) {
+        this.isAnswered = isAnswered
+    }
+}
